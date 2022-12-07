1ed56e79ac4af1dcd5fb42c764bc645b1fbe5bb9
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-12-07 18:51:33 -0500

    implemented Apollo Server in server.js and applied to Express server as middleware

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,32 +1,22 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { Tech, Matchup } = require('../models');
+const { User , Book } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
+  // context
   Query: {
-    getAllTech: async (parent) => {
-      return Tech.find({});
+    me: async (parent, { username }) => {
+      return User.findOne({
+
+      });
     },
-    getAllMatchups: async (parent) => {
-      return Matchup.find({});
-    },
-    getMatchup: async (parent, {matchupId}) => {
-      return Matchup.findOne({ _id: matchupId });
-    }
   },
 
   Mutation: {
-    createMatchup: async (parent, { tech1, tech2 }) => {
-      const matchup = await Matchup.create({ tech1, tech2 })
-      return matchup
-    },
-    createVote: async (parent, { matchupId, tech_num }) => {
-      const matchup = await Matchup.findOneAndUpdate(
-        { _id: matchupId },
-        { $inc: { [`tech${tech_num}_votes`]: 1 } },
-        { new: true }
-      );
-      return matchup
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email })
     }
+
   },
 };
 
