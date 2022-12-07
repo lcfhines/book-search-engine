b68903583643224e34b29b09b456dd7dba6eef82
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-12-07 18:56:37 -0500

    reworked auth.js to pass data via context

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,33 +5,36 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  // receieves object that contains the request
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via  req.body, req.query or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
-
+    // if token can't be verified, return empty request
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
     // verify token and get user data out of it
+    // if token can be verified, add the user's data to the request so it can be accessed in the resolver
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      // return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    // return request object so it can be passed to the resolver
+    return req;
+
   },
   signToken: function ({ username, email, _id }) {
+    // will end up in context
     const payload = { username, email, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
