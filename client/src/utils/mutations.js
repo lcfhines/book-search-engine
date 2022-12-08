7d5e63ff40bd9b388dfb92fc32eab33c06d28035
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-12-08 16:32:22 -0500

    added mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,20 +1,18 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_ME = gql`
-    query me {
-        me {
-            _id
-            username
-            email
-            bookCount
-            savedBooks {
-                bookId
-                authors
-                description
-                title
-                image
-                link
-            }
-        }
-    }
-`
+export const LOGIN_USER = gql`
+
+`;
+
+export const ADD_USER = gql`
+
+`;
+
+export const SAVE_BOOK = gql`
+
+`;
+
+export const REMOVE_BOOK = gql`
+
+`;
+
