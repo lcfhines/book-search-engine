cf17d8852cf778517e8b0a6db4407dec88027d88
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-12-07 19:43:18 -0500

    defined resolver for me query and login mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,17 +5,33 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
   // context
   Query: {
-    me: async (parent, { username }) => {
-      return User.findOne({
-
-      });
+    // add context to the query so we can retrieve the logged-in user without searching for them
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ username: context.user.username })
+      }
+      // throw error if no user retrieved
+      throw new AuthenticationError('You must be logged in!');
     },
   },
 
   Mutation: {
     login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email })
-    }
+      const user = await User.findOne({ email });
+
+      if(!user) {
+        throw new AuthenticationError('No user with this email found!');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password!');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
 
   },
 };
