d736d7d797997074c7410778e750f1ff1c51c9ec
lcfhines <113798073+lcfhines@users.noreply.github.com>
2022-12-08 16:44:56 -0500

    set up SAVE_BOOK and REMOVE_BOOK mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,10 +25,32 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-
+    mutation saveBook($authors: [String]!, $description: String!, $title: String!, $bookId: String!, $image: String!, $link: String!) {
+        saveBook(authors: $authors, description: $description, title: $title, bookId: $bookId, image: $image, link: $link) {
+            _id
+            username
+            bookCount
+            savedBooks {
+                authors
+                description
+                title
+                bookId
+                image
+                link
+            }
+        }
+    }
 `;
 
 export const REMOVE_BOOK = gql`
-
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            username
+            bookCount
+            savedBooks {
+                bookId
+            }
+        }
+    }
 `;
 
